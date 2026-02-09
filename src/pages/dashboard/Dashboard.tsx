c8af8e711090631bea9583948932c52c6b53f105
Karataev Daniil <dirings52445@gmail.com>
2026-02-09 16:11:21 +0200

    fix

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,16 +1,16 @@
-import DashboardHeader from '../../components/Dashboard/DashboardHeader'
-import DashboardSummary from '../../components/Dashboard/DashboardSummary'
-import DashboardRecentActivity from '../../components/Dashboard/DashboardRecentActivity'
+import DashboardHeader from '../../components/dashboard/DashboardHeader'
+import DashboardSummary from '../../components/dashboard/DashboardSummary'
+import DashboardRecentActivity from '../../components/dashboard/DashboardRecentActivity'
 import styles from '../../styles/pages/page.module.css'
 
-import DashboardAnalytics from '../../components/Dashboard/DashboardAnalytics'
-import RevenueCard from '../../components/Dashboard/analytics-cards/RevenueCard'
-import OrderStatusCard from '../../components/Dashboard/analytics-cards/OrderStatusCard'
-import SalesByCategoryCard from '../../components/Dashboard/analytics-cards/SalesByCategoryCard'
+import DashboardAnalytics from '../../components/dashboard/DashboardAnalytics'
+import RevenueCard from '../../components/dashboard/analytics-cards/RevenueCard'
+import OrderStatusCard from '../../components/dashboard/analytics-cards/OrderStatusCard'
+import SalesByCategoryCard from '../../components/dashboard/analytics-cards/SalesByCategoryCard'
 
-import DashboardMetrics from '../../components/Dashboard/DashboardMetrics'
+import DashboardMetrics from '../../components/dashboard/DashboardMetrics'
 // import CancelledOrders from '../../components/dashboard/metrics-cards/CancelledOrders'
-import LowStockAlert from '../../components/Dashboard/metrics-cards/LowStockAlert'
+import LowStockAlert from '../../components/dashboard/metrics-cards/LowStockAlert'
 // import CompletedOrders from '../../components/dashboard/metrics-cards/CompletedOrders'
 
 const Dashboard = ({
